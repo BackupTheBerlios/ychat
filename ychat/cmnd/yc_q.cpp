18a4580d32592d8baafa9d4af5ae0c3164541b10
snooper <snooper>
2003-04-02 10:52:16 +0000

    *** empty log message ***

---
+++ ychat/cmnd/yc_q.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+typedef
+
+using namespace std;
+
+extern "C" {
+ export int function(void *v_arg);
+
+ int function(void *v_arg)
+ {
+  user* p_user = (user*) v_arg; 
+ }
+}
--- ychat/yc_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-extern "C" {
-export int test(char *t);
-
-int test(char *t){
-std::cout << "Hallo" << t << std::endl;
-}
-}
