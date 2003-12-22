44ae41dd299bfce8e832398aa5519cbe533fa9c4
snooper <snooper>
2003-12-22 23:09:35 +0000

    Added make stats

--- ychat_advanced/msgs.h
+++ ychat_advanced/msgs.h
@@ -57,6 +57,7 @@
 #define TIMERAC "Initializing system timer\0"
 #define TIMERUP "Timer: System uptime \0"
 #define VERSION "yChat++ Advanced 0.6-CURRENT (CVS)\0"
-#define BUILDNR 9
+#define BUILDNR 10
+#define UNAME "unknown\0" 
 
 #endif
